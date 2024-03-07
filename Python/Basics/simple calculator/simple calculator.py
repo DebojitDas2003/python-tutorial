5db74d449b85627a0433221d843e3c4f41ddd6ff
DebojitDas2003 <debojitdas0110@gmail.com>
2024-03-08 00:32:21 +0530

    correction done

--- Python/Basics/simple calculator/simple calculator.py
+++ Python/Basics/simple calculator/simple calculator.py
@@ -1,8 +1,8 @@
 '''
 create a simple calculator, that takes two integers and calculate the sum, difference, product, quotient, remainder
 '''
-a=int(input("1st number"))
-b=int(input("2 nd number"))
+a=int(input("1st number: "))
+b=int(input("2nd number: "))
 print("sum=",a+b)
 print("difference=",a-b)
 print("product=",a*b)
