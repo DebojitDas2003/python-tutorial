b8e540924f999aee2b7f86d28b51f68323d35b16
Saloni-biswas <salonibisaws004@gmail.com>
2024-03-08 00:27:07 +0530

    simple calculator python

---
+++ Python/Basics/Hello World/simple calculator/simple calculator.py
@@ -0,0 +1,11 @@
+'''
+create a simple calculator, that takes two integers and calculate the sum, difference, product, quotient, remainder
+'''
+a=int(input("1st number"))
+b=int(input("2 nd number"))
+print("sum=",a+b)
+print("difference=",a-b)
+print("product=",a*b)
+print("quotient=",a/b)
+print("remainder=",a%b)
+
